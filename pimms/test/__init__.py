63339b947a95c889207a490d0a86744221ea0fa6
Noah C. Benson <n@nben.net>
2017-05-04 19:21:49 -0400

    added some tests

--- pimms/test/__init__.py
+++ pimms/test/__init__.py
@@ -15,13 +15,15 @@
 # not, see <http://www.gnu.org/licenses/>.
 
 '''
-The test_pimms test package contains tests for the pimms library as well as examples of the library
+The pimms.test test package contains tests for the pimms library as well as examples of the library
 usage.
 '''
 
-import unittest
-import math
-import pimms
+import unittest, math, sys, pimms
+#from ..immutable import TestPimmsImmutables
+
+if sys.version_info[0] == 3: from   collections import abc as colls
+else:                        import collections            as colls
 
 class TestPimms(unittest.TestCase):
     '''
@@ -45,7 +47,7 @@ class TestPimms(unittest.TestCase):
         self.assertLess(abs(z2.arg - 0.9272952180016122), 1e-9)
         self.assertEqual(z3.abs, 1)
         self.assertLess(abs(z3.arg - (-0.5 * math.pi)), 1e-9)
-
+        
     def test_normal_calc(self):
         from .normal_calc import (normal_distribution, pdf)
         
@@ -89,6 +91,36 @@ class TestPimms(unittest.TestCase):
         self.assertEqual(new_norm_dist_2['variance'], 4)
         #>> Calculating variance...
 
+    _test_map_af = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6}
+    @staticmethod
+    def _make_test_counter_map():
+        count = []
+        def _counter(arg):
+            count.append(arg)
+            return arg
+        def _make_counter(arg):
+            return lambda:_counter(arg)
+        return (count, {k: _make_counter(v) for (k,v) in TestPimms._test_map_af.iteritems()})
+
+    def _assertEquivMaps(self, a, b):
+        self.assertTrue(isinstance(a, colls.Mapping))
+        self.assertTrue(isinstance(b, colls.Mapping))
+        self.assertEqual(len(a), len(b))
+        for (f,s) in [(a,b), (b,a)]:
+            for (k,v) in f.iteritems():
+                self.assertTrue(k in s)
+                self.assertEqual(v, s[k])
+    
+    def test_lazy_basics(self):
+        # First, test that a lazy map works as a normal map:
+        lmap = pimms.lazy_map(TestPimms._test_map_af)
+        self._assertEquivMaps(lmap, TestPimms._test_map_af)
+        # Now a lazy map
+        (c, lmap) = self._make_test_counter_map()
+        lmap = pimms.lazy_map(lmap)
+        print lmap
+        self._assertEquivMaps(lmap, TestPimms._test_map_af)
+        self.assertEqual(set(c), set(TestPimms._test_map_af.values()))
         
 if __name__ == '__main__':
     unittest.main()
