347523f89405eb07d547210ffa4e735326b4eedf
noahbenson <nben@nyu.edu>
2019-03-10 15:09:09 -0400

    upped version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import (setup, Extension)
 
 setup(
     name='pimms',
-    version='0.3.1',
+    version='0.3.2',
     description='Python immutable data structures library',
     keywords='persistent immutable functional',
     author='Noah C. Benson',
