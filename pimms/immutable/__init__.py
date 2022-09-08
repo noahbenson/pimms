3a68ad4fd3f891195ae534bf20a147c65bf0601f
noahbenson <nben@uw.edu>
2022-09-07 20:25:05 -0700

    no longer needed

--- pimms/immutable/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-################################################################################
-# pimms/plantype/__init__.py
-#
-# Definition of the plantype machinery of pimms.
-#
-# By Noah C. Benson
-
-from ._core import (plantype, planobject, is_plantype, is_planobject)
