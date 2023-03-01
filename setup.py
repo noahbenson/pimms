bd5a2e44560f941a472aaaef54c557a3978581a3
Noah Benson <nben@uw.edu>
2023-02-28 19:52:57 -0800

    metadata cleanup

--- setup.py
+++ setup.py
@@ -38,6 +38,8 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
