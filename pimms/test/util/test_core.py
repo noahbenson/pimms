9e3bcf7cf6bbedf127505a17ec8a719fb240a0d7
Noah Benson <nben@uw.edu>
2023-10-05 07:24:14 -0700

    cleanup

--- pimms/test/util/test_core.py
+++ pimms/test/util/test_core.py
@@ -1,32 +1,18 @@
 # -*- coding: utf-8 -*-
 ################################################################################
 # pimms/test/util/test_core.py
-#
-# Tests of the core utilities module in pimms: i.e., tests for the code in the
-# pimms.util._core module.
-#
-# Copyright 2022 Noah C. Benson
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+
+"""Tests of the core utilities module in pimms: i.e., tests for the code in the
+pimms.util._core module.
+"""
+
 
 # Dependencies #################################################################
+
 from unittest import TestCase
+
+
+# Tests ########################################################################
 
 class TestUtilCore(TestCase):
     """Tests the pimms.util._core module."""
